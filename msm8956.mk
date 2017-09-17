523e84589784a2c05bfbd7572adadbe5e10ab08f
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    msm8956-common: Add basic USB HAL that reports no status change
    Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff

--- msm8956.mk
+++ msm8956.mk
@@ -375,7 +375,7 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.basic
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:system/lib64/android.hardware.gnss@1.0-v27.so
