67f455a3957a042300295662077ccd6edb2629c5
drohan007 <drohan940@gmail.com>
2018-08-12 16:01:49 +0000

    msm8956-common: move to passthrough Gatekeeper & Keymaster Hal
    Signed-off-by: drohan007 <drohan940@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -196,7 +196,6 @@ PRODUCT_PACKAGES += \
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service
 
 # GNSS HAL
 PRODUCT_PACKAGES += \
@@ -259,7 +258,6 @@ PRODUCT_COPY_FILES += \
 # Keymaster HAL
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
