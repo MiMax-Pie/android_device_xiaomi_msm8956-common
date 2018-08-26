4ad62dd0b785be5fc5708ad8d5fa4ef7dd3e271b
Amogha Maiya <amoghmaiya@gmail.com>
2018-08-26 09:03:11 +0530

    [DNM] msm8956-common: fix VolTE in reboot device
    * This will clear settings data every reboot for VolTE.

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -477,8 +477,6 @@ case "$buildvariant" in
         ;;
 esac
 
-if [ -f /data/system/users/0/settings_global.xml ]; then
-    sed -i 's/"multi_sim_data_call" value="1"/"multi_sim_data_call" value="-1"/g' 
-/data/system/users/0/settings_global.xml
-    restorecon /data/system/users/0/settings_global.xml
-fi
+#Dirty IMS hack
+
+rm -rf /data/system/users/0
