9027e9e5a41755acb5058d3b06b3b75224c3bf82
TheStrechh <TheStrechh@gmail.com>
2018-08-30 13:52:07 +0000

    msm8956-common: update IMS hack

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -478,6 +478,7 @@ case "$buildvariant" in
 esac
 
 #Dirty IMS hack
-cp -a /data/system/users/0/fpdata /data/system/users/
-rm -rf /data/system/users/0
-cp -a /data/system/users/fpdata /data/system/users/0/
+if [ -f /data/system/users/0/settings_global.xml ]; then
+    sed -i 's/"multi_sim_data_call" value="1"/"multi_sim_data_call" value="-1"/g' /data/system/users/0/settings_global.xml
+    restorecon /data/system/users/0/settings_global.xml
+fi
