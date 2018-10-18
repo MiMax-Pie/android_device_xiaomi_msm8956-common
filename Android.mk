2c3e095670a127efcfa22834d5a1a23ea5d8438e
TARKZiM <tom8476oo@gmail.com>
2018-10-18 17:21:41 +0800

    msm8956-common: add helium

--- Android.mk
+++ Android.mk
@@ -26,7 +26,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter hydrogen kenzo,$(TARGET_DEVICE)),)
+ifneq ($(filter hydrogen helium kenzo,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -37,7 +37,7 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and guards
-write_headers "kenzo hydrogen"
+write_headers "kenzo hydrogen helium"
 
 # The standard common blobs
 write_makefiles "$MY_DIR"/proprietary-files.txt
