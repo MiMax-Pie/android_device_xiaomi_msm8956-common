aeeb389406edd516636ab17fc009be711deefe9d
codejam17 <pratik27pithore@outlook.com>
2018-10-26 18:20:45 +0530

    msm8956: Remove HAX for IMS

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -476,9 +476,3 @@ case "$buildvariant" in
         echo "4 4 1 4" > /proc/sys/kernel/printk
         ;;
 esac
-
-#Dirty IMS hack
-if [ -f /data/system/users/0/settings_global.xml ]; then
-    sed -i 's/"multi_sim_data_call" value="1"/"multi_sim_data_call" value="-1"/g' /data/system/users/0/settings_global.xml
-    restorecon /data/system/users/0/settings_global.xml
-fi
