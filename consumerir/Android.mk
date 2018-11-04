c9a65d98abf330cff83773ae141991919bc7dcf3
TheStrechh <TheStrechh@gmail.com>
2018-11-03 18:49:24 -0700

    msm8956-common: Update copyrights for LineageOS

--- consumerir/Android.mk
+++ consumerir/Android.mk
@@ -1,4 +1,6 @@
-# Copyright (C) 2013 The Android Open Source Project
+#
+# Copyright (C) 2015-2016 The CyanogenMod Project
+#           (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 LOCAL_PATH := $(call my-dir)
 
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2015-2016 The CyanogenMod Project
+#           (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 
 set -e
 
