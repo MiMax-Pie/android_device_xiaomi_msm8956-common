2822ef91a9f3620c22ac4088661536f4232ca734
TARKZiM <tom8476oo@gmail.com>
2019-01-25 15:54:51 +0800

    msm8956-common: include QCOM device sepolicies

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -220,7 +220,7 @@ PROTOBUF_SUPPORTED := true
 TARGET_RIL_VARIANT := caf
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
 #include device/qcom/sepolicy-legacy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
